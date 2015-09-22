80273f0266cf5f6fec55a8d78b58a1cfac25a2f3
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2015-09-22 17:45:18 +0530

    pydub changes and new binary with better install.sh. Binary has not been updated.... Wait until some functionality is available for binaries.

--- raspbian-music.py
+++ raspbian-music.py
@@ -1,15 +1,34 @@
-import subprocess
+import subprocess, sys
+from pydub import AudioSegment
+from pydub.playback import play
 
-def omxplayer_control(file, args, ignite=True):
-    global omxplayer
-    if ignite == True:
-        omxplayer = subprocess.Popen(["omxplayer", args, file], universal_newlines=True)
-    elif ignite == False:
-        omxplayer.kill()
+def get_format(path=False, file=False):
+    if path != False:
+        if sys.platform == "linux":
+            path_split = path.split('/')
+        elif sys.platform == "win32":
+            path_split = path.split('\\')
+        tmp = path_split[len(path_split) - 1]
+    elif file != False:
+        tmp = file
+    tmp1 = tmp.split('.')
+    format = tmp1[len(tmp) - 1]
+    return format
 
+def play_file(path):
+    format = get_format()
+    if format == "mp3":
+        song_process = AudioSegment.from_mp3(path)
+    elif format == "ogg":
+        song_process = AudioSegment.from_ogg(path)
+    elif format == "flv":
+        song_process = AudioSegment.from_flv(path)
+    elif format == "wav":
+        song_process = AudioSegment.from_wav(path)
+    else:
+        try:
+            song_process = AudioSegment.from_file(path)
+        except:
+            return False
+    play(song_process)
 
-def omxplayer_communication(command="play"):
-    if command == "play" or command == "pause":
-        omxplayer.communicate("p")
-    elif command == "stop":
-        omxplayer.kill()
