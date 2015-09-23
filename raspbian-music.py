f6fc6028456c113faa42ede8a54672ae0c460b12
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2015-09-23 17:45:44 +0530

    Huge pyglet changes and renovation...Unfortunately this commit stalls the entire project.

--- raspbian-music.py
+++ raspbian-music.py
@@ -1,34 +1,23 @@
-import subprocess, sys
-from pydub import AudioSegment
-from pydub.playback import play
+# Raspbian Music, a music player for Linux-based ARM devices (mainly RPi)
+# Copyright (C) 2015 Ibrahim Ansari
 
-def get_format(path=False, file=False):
-    if path != False:
-        if sys.platform == "linux":
-            path_split = path.split('/')
-        elif sys.platform == "win32":
-            path_split = path.split('\\')
-        tmp = path_split[len(path_split) - 1]
-    elif file != False:
-        tmp = file
-    tmp1 = tmp.split('.')
-    format = tmp1[len(tmp) - 1]
-    return format
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
 
-def play_file(path):
-    format = get_format()
-    if format == "mp3":
-        song_process = AudioSegment.from_mp3(path)
-    elif format == "ogg":
-        song_process = AudioSegment.from_ogg(path)
-    elif format == "flv":
-        song_process = AudioSegment.from_flv(path)
-    elif format == "wav":
-        song_process = AudioSegment.from_wav(path)
-    else:
-        try:
-            song_process = AudioSegment.from_file(path)
-        except:
-            return False
-    play(song_process)
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Importing modules
+import pyglet
+
+# Initialize player
+player = pyglet.media.Player()
+
+## GUI coming soon.
